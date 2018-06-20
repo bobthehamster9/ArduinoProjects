96faf630077c504abc76ab55bc6312ffae1b84bb
Jack Fountain <jackdkdf@gmail.com>
2018-06-20 14:13:30 -0400

    Added wind to weather station

--- WeatherStation/WeatherStation.ino
+++ WeatherStation/WeatherStation.ino
@@ -2,7 +2,7 @@
 #include <OneWire.h>
 #include <DallasTemperature.h>
 #include <LiquidCrystal.h>
-#include <dht.h>s
+#include <dht.h>
 
 
 // Data wire is plugged into pin 2 on the Arduino
@@ -18,14 +18,20 @@ dht DHT;
 
 #define DHT11_PIN 9
 
+const int hallPin=12;
+const unsigned long sampleTime=1000;
+const int maxRPM = 10200; 
+
 void setup(void)
 {
   sensors.begin();
   lcd.begin(16,2);
+  pinMode(hallPin,INPUT);
 }
 
 void loop(void)
 { 
+  int rpm=getRPM();
   int chk = DHT.read11(DHT11_PIN);
   // Send the command to get temperatures
   sensors.requestTemperatures();  
@@ -39,10 +45,39 @@ void loop(void)
   lcd.setCursor(6, 0);
   lcd.print(sensors.getTempCByIndex(0));
   lcd.setCursor(0, 1);
-  lcd.print("Humidity:");
-  lcd.setCursor(9, 1);
+  lcd.print("Hum:");
+  lcd.setCursor(4, 1);
   lcd.print(DHT.humidity);
-  lcd.setCursor(14, 1);
+  lcd.setCursor(8, 1);
   lcd.print("%");
-  delay(50);
+  lcd.setCursor(10, 1);
+  lcd.print("km:");
+  lcd.setCursor(13, 1);
+  lcd.print(10 * rpm * 0.001885);
+  delay(100);
+}
+
+int getRPM()
+{
+  // sample for sampleTime in millisecs
+  int kount=0;
+  boolean kflag=LOW;
+  unsigned long currentTime=0;
+  unsigned long startTime=millis();
+  while (currentTime<=sampleTime)
+  {
+    if (digitalRead(hallPin)==HIGH)
+    {
+      kflag=HIGH;
+    }
+    if (digitalRead(hallPin)==LOW && kflag==HIGH)
+    {
+      kount++;
+      kflag=LOW;
+    }
+    currentTime=millis()-startTime;
+  }
+  int kount2rpm = int(60000./float(sampleTime))*kount;
+  //int rpm2km = int(1 * kount2rpm * 0.001885);
+  return kount2rpm;
 }
